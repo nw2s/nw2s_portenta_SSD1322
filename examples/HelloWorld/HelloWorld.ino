b404a7caa1c394b41c5e74bbddea46b1d06e1b00
Scott Wilson <sw@nw2s.net>
2022-12-28 08:05:24 -0500

    finished char mapping

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -2,13 +2,15 @@
 #include <ssd1322.h>
 
 uint8_t frame_buffer[8192];
-
+ 
+char char_counter = 0;
+ 
 void setup()
 {
     Serial.begin(115200);
-    while (!Serial);
+    // while (!Serial);
 
-	delay(5000);
+	delay(2000);
 	
 	SPI.begin();
 
@@ -18,18 +20,30 @@ void setup()
 
 	Serial.println("Clear display RAM");
     ssd1322_fill_ram(0x00);
+	
+    ssd1322_fill_fb(frame_buffer, 0x00);
+    ssd1322_display_fb(frame_buffer);
 
+	delay(500);
 }
 
 void loop()
 {
-    ssd1322_fill_fb(frame_buffer, 0xFF);
-    ssd1322_display_fb(frame_buffer);
-
-    delay(500);
-
     ssd1322_fill_fb(frame_buffer, 0x00);
+
+	for (uint8_t i = 0; i < 3; ++i)
+	{
+		for (uint8_t j = 0; j < 19; ++j)
+		{
+			draw_char_on_framebuffer(frame_buffer, char_counter + 32, j, i);			
+		}
+	}
     ssd1322_display_fb(frame_buffer);
-	
-	delay(500);
+
+	char_counter = (char_counter + 1) % 102;
+		
+    delay(100);
 }
+
+
+
